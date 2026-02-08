37ab510a44c60e9ea0e70e28b4a012e05397838b
DuyPV3 <duypv3@fpt.com>
2026-02-08 15:23:00 +0700

    update

--- config/telegram.php
+++ config/telegram.php
@@ -1,6 +1,6 @@
 <?php
 $telegramConfig = [
-    'bot_token' => 'YOUR_BOT_TOKEN_HERE', // Thay bằng Token Bot của bạn (lấy từ @BotFather)
-    'chat_id' => 'YOUR_CHAT_ID_HERE',     // Thay bằng Chat ID của bạn (lấy từ @userinfobot hoặc getUpdates)
+    'bot_token' => '8565636416:AAG27q-QnsbPmzRNIeIOyMF7ieghgfOx9Ns', // Thay bằng Token Bot của bạn (lấy từ @BotFather)
+    'chat_id' => '-1003880859687',     // Thay bằng Chat ID của bạn (lấy từ @userinfobot hoặc getUpdates)
 ];
 ?>
